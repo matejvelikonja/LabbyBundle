70dff0e801f5959512f507e1fa403326d1e44579
matejvelikonja <matej@velikonja.si>
2014-11-06 00:30:23 +0100

    more verbose output

--- Database/SyncerDb.php
+++ Database/SyncerDb.php
@@ -2,6 +2,7 @@
 
 namespace Velikonja\LabbyBundle\Database;
 
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Velikonja\LabbyBundle\Command\DumpCommand;
 use Velikonja\LabbyBundle\Remote\Ssh;
@@ -35,6 +36,8 @@ class SyncerDb
      * @param Ssh           $ssh
      * @param Scp           $scp
      * @param ZipArchive    $zip
+     *
+     * @throws \Exception
      */
     public function __construct(MySqlImporter $importer, Ssh $ssh, Scp $scp, ZipArchive $zip)
     {
@@ -42,6 +45,13 @@ class SyncerDb
         $this->ssh      = $ssh;
         $this->scp      = $scp;
         $this->zip      = $zip;
+        $this->tmpDir  = sys_get_temp_dir();
+
+        if (! is_writable($this->tmpDir)) {
+            throw new \Exception(
+                sprintf('Temporary directory `%s` is unreadable.', $this->tmpDir)
+            );
+        }
     }
 
     /**
@@ -50,10 +60,13 @@ class SyncerDb
      * @throws DatabaseException
      * @throws \Exception
      */
-    public function sync(OutputInterface $output)
+    public function sync(OutputInterface $output = null)
     {
-        $remoteFile = 'dump-remote.zip';
-        $localFile  = 'dump-local.zip';
+        $output     = $output ?: new NullOutput();
+        $remoteFile = 'dump-remote.zip'; //TODO: improve location and name of remote file
+        $localFile  = $this->tmpDir . '/labby_' . uniqid() . '.zip';
+
+        $output->writeln('Executing remote dump..');
 
         $this->ssh->execSf(
             DumpCommand::COMMAND_NAME,
@@ -64,15 +77,40 @@ class SyncerDb
             $output
         );
 
+        $output->writeln('Copying...');
         $this->scp->copyFile($remoteFile, $localFile, $output);
+        $output->writeln(
+            sprintf(
+                'Remote file `%s` copied locally to `%s`.',
+                $remoteFile,
+                $localFile
+            )
+        );
 
-        $dump = $this->zip->unzip($localFile);
+        $dumpFile = $this->zip->unzip($localFile);
+        $output->writeln(
+            sprintf(
+                'File `%s` unzipped to `%s`.',
+                $localFile,
+                $dumpFile
+            )
+        );
 
-        $this->importer->import($dump);
+        $this->importer->import($dumpFile);
+        $output->writeln(
+            sprintf(
+                'File `%s` imported.',
+                $dumpFile
+            )
+        );
 
-        // cleanup
+        $output->writeln('Cleaning up...');
+
         unlink($localFile);
-        unlink($dump);
+        $output->writeln(sprintf('File `%s` removed.', $localFile));
+
+        unlink($dumpFile);
+        $output->writeln(sprintf('File `%s` removed.', $dumpFile));
 
         //TODO: remove remote file
     }
