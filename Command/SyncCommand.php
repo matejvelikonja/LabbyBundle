ff7dc98d8f2ddbfdfa77b59e77ae408036b97f9f
matejvelikonja <matej@velikonja.si>
2015-01-08 13:28:32 +0100

    command to sync db and fs in one run

---
+++ Command/SyncCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Velikonja\LabbyBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+class SyncCommand extends BaseCommand
+{
+    const COMMAND_NAME = 'labby:sync';
+
+    /**
+     * Configure command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::COMMAND_NAME)
+            ->setDescription('Run synchronization of filesystem maps and DB.')
+            ->setRoles(array(self::ROLE_LOCAL));
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @throws \Exception
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $syncer    = $this->getContainer()->get('velikonja_labby.service.syncer');
+        $stopwatch = new Stopwatch();
+
+        $stopwatch->start('sync');
+        $syncer->syncDb($output);
+        $syncer->syncFs($output);
+        $event = $stopwatch->stop('sync');
+
+        $output->writeln('');
+        $output->writeln(
+            sprintf(
+                '<info>Finished in %.2f seconds!</info>',
+                $event->getDuration() / 1000
+            )
+        );
+    }
+}
