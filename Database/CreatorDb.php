f0418ecf9e9fa8acb4d8f71024fbf1bfa60c876d
matejvelikonja <matej@velikonja.si>
2015-03-19 00:22:24 +0100

    use nulloutput for sf 2.3

--- Database/CreatorDb.php
+++ Database/CreatorDb.php
@@ -3,6 +3,7 @@
 namespace Velikonja\LabbyBundle\Database;
 
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Velikonja\LabbyBundle\Executor\SymfonyCommandExecutor;
 
@@ -27,7 +28,7 @@ class CreatorDb
     public function create(OutputInterface $output = null)
     {
         $error     = false;
-        $outputTmp = null;
+        $outputTmp = new NullOutput();
 
         // SF 2.3 does not have BufferedOutput
         if (class_exists('Symfony\Component\Console\Output\BufferedOutput')) {
@@ -41,7 +42,7 @@ class CreatorDb
             $error = true;
         }
 
-        if (! $error && $outputTmp) {
+        if (! $error && method_exists($outputTmp, 'fetch')) {
             $output->write($outputTmp->fetch());
         }
 
