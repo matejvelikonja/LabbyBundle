8ec01b7caa43cd3027efa78c63b364c8d726a851
matejvelikonja <matej@velikonja.si>
2015-03-18 21:52:09 +0100

    don't throw exception if database does not exists

--- Database/CreatorDb.php
+++ Database/CreatorDb.php
@@ -2,6 +2,7 @@
 
 namespace Velikonja\LabbyBundle\Database;
 
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Velikonja\LabbyBundle\Executor\SymfonyCommandExecutor;
 
@@ -25,7 +26,20 @@ class CreatorDb
      */
     public function create(OutputInterface $output = null)
     {
-        $this->executor->execute('doctrine:database:drop --force', $output);
+        $bufferedOutput = new BufferedOutput(OutputInterface::VERBOSITY_NORMAL, true);
+        $error          = false;
+
+        try {
+            $this->executor->execute('doctrine:database:drop --force', $bufferedOutput);
+        } catch (\RuntimeException $e) {
+            // catches an error if database does not exist
+            $error = true;
+        }
+
+        if (! $error) {
+            $output->write($bufferedOutput->fetch());
+        }
+
         $this->executor->execute('doctrine:database:create', $output);
     }
 }
